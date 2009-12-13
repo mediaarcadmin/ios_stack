f07daf4096ae3dc2d2130cc1bc88320f8a5339aa
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2009-12-13 18:52:16 +0000

    More work on layout algorithm.

--- Source/EucHTMLDocumentNode.h
+++ Source/EucHTMLDocumentNode.h
@@ -7,6 +7,7 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <libcss/libcss.h>
 
 @class EucHTMLDBNode, EucHTMLDocument;
 
--- Source/EucHTMLDocumentRenderer.m
+++ Source/EucHTMLDocumentRenderer.m
@@ -7,32 +7,80 @@
 //
 
 #import "EucHTMLDocumentRenderer.h"
-
+#import "EucHTMLDocumentNode.h"
 
 @implementation EucHTMLDocumentRenderer
 
+/*
+Return words, 
+Line start x positions and widths,
+Line end word and hyphenation points, 
+Block contents x/y position.
+Block completion status.
+*/
 
-
-- (void)layout
+- (BOOL)layoutNode:(EucHTMLDocumentNode *)node 
+               atX:(uint32_t)x
+               atY:(uint32_t)y
+   maxContentWidth:(uint32_t)width 
+  maxContentHeight:(uint32_t)height
+      laidOutNodes:(NSMutableArray *)laidOutNodes
+     laidOutFloats:(NSMutableArray *)laidOutFloats
 {
 /*
+    Keep track of: 
+        Fully laid out child nodes.
+        X/Y of start of current block content
+        Current margin border and padding.
+        Stack of left floats with end y position and width.
+        Stack of right floats with end y position and width.
+        "Current" line and line's max height so far.
+        Node/Word/Break position of start of lines in current block.
+ 
     While we have subnodes
+ 
         If subnode is block
-            If line has contents, start new line.
+            Add previous block to laid out children list.
+            Start new line (take into account "clear").
+            Performing margin collapsing.
+            If content of block is over the end of the page
+                Return.
+ 
         If subnode is inline
             While subnodes are inline and not float
-                Combine contents and perform whitespace processing.
-                Need to be able to map from processed words back
-                to their original nodes for sizing and rendering purposes.
+                Combine contents, performing whitespace processing.
+                    Create array of words *and spaces*.  
+                    Parallel array maps to node id.
+                    Second parallel array maps to hyphenation points.
             Create horizontally-sized valid-break structure.
-                // Remember to take into account indentation and any contents
-                // already on the line.
+                Remember to take into account indentation and any contents
+                already on the line.
             Justify resulting paragraph.
-                Can we save the placement of the words at this point?]
-                need to take into account baseline...
-                Don't think it's possible, since we may need to expand the 
-                line height based on ontent after a float. 
-            If last line is over page return page done code (with place).
+            Store word + hyphenation point -> line end map.
+                Work out line heights.
+            If floats end before page ends resulting lines overlap float end positions
+                Pop and reflow from start of line after overlap stops.
+            If resulting lines overflow the bottom of the page
+                If (overflow count < allowed widows count) 
+                    If (non-overflowed count - (allowed widows count - overflow count)) > orphans count
+                        Record node position.
+                        Return start of line at (non-overflowed count - (allowed widows count - overflow count))
+                    Else
+                        Remove any content from this block that's on this page.
+                        Return start of block.
+                Record node to overflow position in laid out list.
+                Record laid out children in laid out list.
+                Return.
+            If all lines fit
+                Check bottom padding of element.
+                If bottom padding < remaining height
+                    If (lines in block - min widows height) > min. orphans count.
+                        Record node to (lines in block - min widows height) in laid out list.
+                        Record laid out children in laid out list.
+                        Return.
+                    Else
+                        Record laid out children in laid out list.
+                        Return.
         Else if subnode is float
             Recurse to lay out float in available width.
             -- No need to worry about block vs. inline - that's 
@@ -40,15 +88,29 @@
             If resulting float is smaller than available width
                 Place resulting float on current line.
             Else
-                Place resulting float on next line.
+                If float fits in empty line width
+                    Place resulting float on next line.
+                else
+                    Place float below any current floats.
+            If float doesn't fit on page vertically
+                If it's not on the first line.
+                    Record node to start of the current line in laid out list.
+                    Record laid out children in laid out list.
+                    Return.
+            Record float in laid out floats list.
+            Update float stack.
         Else if subnode is block
-            Start new line
             Recurse with block in available width.
+            If block not fully laid out
+                Record node position to end of current line in laid out list.
+                Record laid out children in laid out list.
+                Record partially laid out child.
+                Return.
  
-    At what points can we test to see if we're over the requisite height?
-    after each justification?  May give false-negaties (but that's okay - we'll
-    re-process the line after completing the float).
-*/
+    Return success
+ */
+    
+    return NO;
 }
 
 
