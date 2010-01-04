2297a713c751e201ecc135d11c1575b1e9991123
Arnold Chien <arnoldchien@arnold-chiens-macbook-pro.local>
2010-01-04 14:36:13 -0500

    New license files, enables speech without the stupid eval messages.  The compiler complains about "backslash/newline at end of file" for babkurz.lic.h, which is bogus according to my hex editor.  Requires turning off "Treat warnings as errors".

---
+++ Sources/Classes/TTS/Acapela/babkurz.lic.h
@@ -0,0 +1,5 @@
+const char babLicense[]="\"9147 0 KURZ #COMMERCIAL#Kurzweil National Federation of the Blind 15 Walnut Street MA 02481 Wellesley Hills MA USA\"\n\
+ZGSZDqZlhg$Ri7pSiuty8oBBgdn6Xmhlf5bnIIX4YlHiWKflvw!Ksm3TydRgldkTyB9V9Tn%hrqBJDo5DqV@L65SJjcT55WSDmtBQvGnrHOzM6AIiu8Ss%Ca4wGRdO2Bj9PW4ufkzTtuyBtFVyI7nNTTVS##\n\
+WGGBjbVk$gen2NoNAY6%6kl7tLL$jWfuw4H$\n\
+XyHii9L@YkZlOfqJPAWVnT##\n\
+";
