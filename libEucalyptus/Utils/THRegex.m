a128f130475c5da62fbca466e6a2be0bb1717cc5
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2010-01-30 11:09:49 +0000

    Open source THRegex.

--- libEucalyptus/Utils/THRegex.m
+++ libEucalyptus/Utils/THRegex.m
@@ -1,8 +1,37 @@
 //
 //  THRegex.m
+//  http://www.blog.montgomerie.net/using-the-mac-or-iphones-built-in-regex
 //
 //  Created by James Montgomerie on 22/05/2008.
-//  Copyright 2008-2009 James Montgomerie. All rights reserved.
+//  jamie@th.ingsmadeoutofotherthin.gs
+//  http://www.blog.montgomerie.net/
+//
+//  Copyright 2008-2009 Things Made Out Of Other Things Ltd.
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright notice, 
+//    this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//  * Neither the name of the Things Made Out Of Other Things Ltd., nor the
+//    names of its contributors may be used to endorse or promote products 
+//    derived from this software without specific prior written permission.
+// 
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
 //
 
 #import "THRegex.h"
@@ -35,6 +64,12 @@
 {
     regfree(&_regex);
     [super dealloc];
+}
+
+- (void)finalize
+{
+    regfree(&_regex);
+    [super finalize];
 }
 
 + (id)regexDataWithPOSIXRegex:(NSString *)regexString flags:(int)flags
@@ -104,7 +139,6 @@
     return [self initWithPOSIXRegex:regex flags:REG_EXTENDED];
 }
 
-
 + (id)regexWithPOSIXRegex:(NSString *)regexString
 {
     return [[[self alloc] initWithPOSIXRegex:regexString flags:REG_EXTENDED] autorelease];
@@ -118,13 +152,17 @@
 - (void)dealloc
 {
     [_regexData release];
-    if(_matches) {
-        free(_matches);
-    }
-    if(_UTF8buffer) {
-        free(_UTF8buffer);
-    }
+    free(_matches);
+    free(_UTF8buffer);
+    
     return [super dealloc];
+}
+
+- (void)finalize
+{
+    free(_matches);
+    free(_UTF8buffer);
+    [super finalize];
 }
     
 
