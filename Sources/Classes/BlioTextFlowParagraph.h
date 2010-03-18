b59521d3de751f6b3733f4a2dc7c4563d3af27a0
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2010-03-18 21:37:35 +0000

    First padd at a paragraph parser - not hooked up yet, but should build a EucCSS compatible tree from a flow file.

--- Sources/Classes/BlioTextFlowParagraph.h
+++ Sources/Classes/BlioTextFlowParagraph.h
@@ -7,10 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <libEucalyptus/EucCSSDocumentTreeNode.h>
 
+@class BlioTextFlow, BlioTextFlowFlow, BlioTextFlowParagraphWords;
 
-@interface BlioTextFlowParagraph : NSObject {
-
+@interface BlioTextFlowParagraph : NSObject <EucCSSDocumentTreeNode> {
+    uint32_t _key;
+    
+    // Nothing below is retained.
+    BlioTextFlow *_textFlow; 
+    
+    BlioTextFlowFlow *_parent;
+    BlioTextFlowParagraph *_previousSibling;
+    BlioTextFlowParagraph *_nextSibling;
+    
+    BlioTextFlowParagraphWords *_paragraphWords;
 }
+
+@property (nonatomic, assign, readonly) BlioTextFlow *textFlow;
+@property (nonatomic, assign) BlioTextFlowParagraphWords *paragraphWords;
+
+@property (nonatomic, assign) BlioTextFlowParagraph *previousSibling;
+@property (nonatomic, assign) BlioTextFlowParagraph *nextSibling;
+
+- (id)initWithTextFlow:(BlioTextFlow *)textFlow 
+              flowNode:(BlioTextFlowFlow *)flowNode
+                   key:(uint32_t)key;
 
 @end
---
+++ Sources/Classes/BlioTextFlowParagraphWords.h
@@ -0,0 +1,31 @@
+//
+//  BlioTextFlowParagraphWords.h
+//  BlioApp
+//
+//  Created by James Montgomerie on 18/03/2010.
+//  Copyright 2010 Things Made Out Of Other Things. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <libEucalyptus/EucCSSDocumentTreeNode.h>
+
+@class BlioTextFlowParagraph, BlioTextFlow, BlioBookmarkPoint;
+
+@interface BlioTextFlowParagraphWords : NSObject <EucCSSDocumentTreeNode> {
+    BlioTextFlow *_textFlow;           // nonretained.
+    BlioTextFlowParagraph *_paragraph; // nonretained.
+    
+    NSArray *_ranges;
+    uint32_t _key;
+}
+
+- (id)initWithTextFlow:(BlioTextFlow *)textFlow
+             paragraph:(BlioTextFlowParagraph *)paragraph 
+                ranges:(NSArray *)ranges
+                   key:(uint32_t)key;
+
+- (BlioBookmarkPoint *)wordOffsetToBookmarkPoint:(uint32_t)wordOffset;
+- (NSArray *)wordStrings;
+- (NSArray *)words;
+
+@end
