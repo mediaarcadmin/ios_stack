5ef3c2c6a14d816e177d92e5cd425bbe9b9c609b
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2010-03-31 19:00:34 +0100

    Fix up project to include cache files in libEucalyptusCSS (was only compiling them into the paginate target by mistake).

--- libEucalyptus/Utils/THTreeCache.h
+++ libEucalyptus/Utils/THTreeCache.h
@@ -6,7 +6,7 @@
 //  Copyright 2010 Things Made Out Of Other Things. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import "pthread.h"
 
 @interface THTreeCache : NSObject {
