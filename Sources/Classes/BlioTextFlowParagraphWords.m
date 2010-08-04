7cbb754ab3ad09a5a87b6fc027df34763c3fda2b
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2010-08-04 10:27:13 +0100

    Compile with hyperlink-aware libEucalyptus.  Fix one issue with a duplicate file in the project.

--- Sources/Classes/BlioTextFlowParagraphWords.m
+++ Sources/Classes/BlioTextFlowParagraphWords.m
@@ -158,7 +158,17 @@
     return NO;
 }
 
-- (NSString *)imageSourceURL
+- (NSString *)imageSourceURLString
+{
+    return nil;
+}
+
+- (BOOL)isHyperlinkNode
+{
+    return NO;
+}
+
+- (NSString *)hyperlinkURLString
 {
     return nil;
 }
