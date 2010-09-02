6810676770bc249f702c8a014cc85d72b502bde3
Matt Farugia <matt.farrugia@bitwink.com>
2010-09-02 08:29:02 +0100

    Removed legacy LayoutView classes and methods

--- Sources/Classes/BlioLayoutView.h
+++ Sources/Classes/BlioLayoutView.h
@@ -16,27 +16,7 @@
 #import "BlioXPSProvider.h"
 #import <libEucalyptus/EucPageTurningView.h>
 
-static const NSUInteger kBlioLayoutMaxPages = 6; // Must be at least 6 for the go to animations to look right
-
-@class BlioLayoutScrollView;
-@class BlioLayoutContentView;
-@class BlioLayoutPageLayer;
-
-typedef enum BlioLayoutPageMode {
-    BlioLayoutPageModePortrait,
-    BlioLayoutPageModeLandscape
-} BlioLayoutPageMode;
-
-@protocol BlioLayoutRenderingDelegate
-- (BOOL)dataSourceContainsPage:(NSInteger)page;
-- (void)drawThumbLayer:(CALayer *)aLayer inContext:(CGContextRef)ctx forPage:(NSInteger)page withCacheLayer:(CGLayerRef)cacheLayer;
-- (void)drawTiledLayer:(CALayer *)aLayer inContext:(CGContextRef)ctx forPage:(NSInteger)page cacheReadyTarget:(id)target cacheReadySelector:(SEL)readySelector;
-- (void)drawShadowLayer:(CALayer *)aLayer inContext:(CGContextRef)ctx forPage:(NSInteger)page;
-- (void)drawHighlightsLayer:(CALayer *)aLayer inContext:(CGContextRef)ctx forPage:(NSInteger)page excluding:(BlioBookmarkRange *)excludedBookmark;
-
-@end
-
-@interface BlioLayoutView : BlioSelectableBookView <EucPageTurningViewDelegate, EucPageTurningViewBitmapDataSource, BlioLayoutRenderingDelegate, UIScrollViewDelegate, BlioBookView, EucSelectorDataSource, EucSelectorDelegate> {
+@interface BlioLayoutView : BlioSelectableBookView <EucPageTurningViewDelegate, EucPageTurningViewBitmapDataSource, BlioBookView, EucSelectorDataSource, EucSelectorDelegate> {
     NSManagedObjectID *bookID;
     EucPageTurningView *pageTurningView;
     UIImage *pageTexture;
@@ -44,40 +24,15 @@ typedef enum BlioLayoutPageMode {
     
     BlioTextFlow *textFlow;
     CGPDFDocumentRef pdf;
-    BlioLayoutScrollView *scrollView;
-    UIView *containerView;
-    BlioLayoutContentView *contentView;
-    NSInteger visiblePageIndex;
-    BlioLayoutPageLayer *currentPageLayer;
-    BOOL scrollingAnimationInProgress;
     NSInteger pageNumber;
     NSInteger pageCount;
-    CGFloat lastZoomScale;
-    CGFloat targetZoomScale;
-    CGPoint targetContentOffset;
-    BOOL shouldZoomOut;
-    CALayer *sharpLayer;
     EucSelector *selector;
     NSMutableDictionary *pageCropsCache;
     NSMutableDictionary *viewTransformsCache;
     CGRect firstPageCrop;
-    UIImage *checkerBoard;
-    UIImage *shadowBottom;
-    UIImage *shadowTop;
-    UIImage *shadowLeft;
-    UIImage *shadowRight;
     UIImage *pageSnapshot;
     UIImage *highlightsSnapshot;
-    BOOL isCancelled;
-    BlioTextFlowBlock *lastBlock;
-    BlioLayoutPageMode layoutMode;
-    CGAffineTransform cachedViewTransform;
-    NSInteger cachedViewTransformPage;
-    NSMutableArray *accessibilityElements;
-    BlioTimeOrderedCache *accessibilityCache;
-    
-    BOOL accessibilityRefreshRequired;
-    
+       
     BlioXPSProvider *xpsProvider;
     id<BlioLayoutDataSource> dataSource;
     
@@ -87,21 +42,10 @@ typedef enum BlioLayoutPageMode {
 @property (nonatomic, retain) NSManagedObjectID *bookID;
 @property (nonatomic, retain) EucPageTurningView *pageTurningView;
 
-
 @property (nonatomic, retain) BlioTextFlow *textFlow;
-@property (nonatomic, retain) BlioLayoutScrollView *scrollView;
-@property (nonatomic, retain) UIView *containerView;
-@property (nonatomic, retain) BlioLayoutContentView *contentView;
-@property (nonatomic, retain) BlioLayoutPageLayer *currentPageLayer;
-@property (nonatomic) BOOL scrollingAnimationInProgress;
 @property (nonatomic, readonly) NSInteger pageNumber;
 @property (nonatomic, retain) EucSelector *selector;
 @property (nonatomic, retain) NSMutableDictionary *pageCropsCache;
 @property (nonatomic, retain) NSMutableDictionary *viewTransformsCache;
-@property (nonatomic, retain) UIImage *checkerBoard;
-@property (nonatomic, retain) UIImage *shadowBottom;
-@property (nonatomic, retain) UIImage *shadowTop;
-@property (nonatomic, retain) UIImage *shadowLeft;
-@property (nonatomic, retain) UIImage *shadowRight;
 
 @end
