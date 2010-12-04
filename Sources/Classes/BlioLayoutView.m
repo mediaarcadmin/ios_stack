3bee9c2f452894ec2b080c4e95eca1b6abed538b
Arnold Chien <arnoldchien@arnold-chiens-macbook-pro.local>
2010-12-04 13:49:18 -0500

    Fix 255

--- Sources/Classes/BlioLayoutView.m
+++ Sources/Classes/BlioLayoutView.m
@@ -1554,25 +1554,7 @@ CGAffineTransform transformRectToFitRect(CGRect sourceRect, CGRect targetRect, B
     if(!accessibilityElements) {
         accessibilityElements = [[NSMutableArray arrayWithArray:[self textBlockAccessibilityElements]] retain];
 
-        CGFloat tapZoneWidth = 0.1f * self.pageTurningView.bounds.size.width;
-		
-        {
-            UIAccessibilityElement *nextPageTapZone = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self];
-            nextPageTapZone.accessibilityTraits = UIAccessibilityTraitButton;
-            if (self.pageTurningView.rightPageIndex >= (self.pageCount - 1))  {
-                nextPageTapZone.accessibilityTraits |= UIAccessibilityTraitNotEnabled;
-            }            
-            CGRect frame = self.pageTurningView.bounds;
-            frame.origin.x = frame.size.width + frame.origin.x - tapZoneWidth;
-            frame.size.width = tapZoneWidth;
-			frame = [self.window.layer convertRect:frame fromLayer:self.pageTurningView.layer];
-
-            nextPageTapZone.accessibilityFrame = frame;
-            nextPageTapZone.accessibilityLabel = NSLocalizedString(@"Next Page", @"Accessibility label for next page tap zone");
-			self.nextZone = nextPageTapZone;
-            [accessibilityElements addObject:nextPageTapZone];            
-            [nextPageTapZone release];
-        }        
+        CGFloat tapZoneWidth = 0.1f * self.pageTurningView.bounds.size.width;      
         
         {
             UIAccessibilityElement *previousPageTapZone = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self];
@@ -1616,6 +1598,24 @@ CGAffineTransform transformRectToFitRect(CGRect sourceRect, CGRect targetRect, B
             [accessibilityElements addObject:toolbarTapButton];
             [toolbarTapButton release];
         }
+		
+        {
+            UIAccessibilityElement *nextPageTapZone = [[UIAccessibilityElement alloc] initWithAccessibilityContainer:self];
+            nextPageTapZone.accessibilityTraits = UIAccessibilityTraitButton;
+            if (self.pageTurningView.rightPageIndex >= (self.pageCount - 1))  {
+                nextPageTapZone.accessibilityTraits |= UIAccessibilityTraitNotEnabled;
+            }            
+            CGRect frame = self.pageTurningView.bounds;
+            frame.origin.x = frame.size.width + frame.origin.x - tapZoneWidth;
+            frame.size.width = tapZoneWidth;
+			frame = [self.window.layer convertRect:frame fromLayer:self.pageTurningView.layer];
+			
+            nextPageTapZone.accessibilityFrame = frame;
+            nextPageTapZone.accessibilityLabel = NSLocalizedString(@"Next Page", @"Accessibility label for next page tap zone");
+			self.nextZone = nextPageTapZone;
+            [accessibilityElements addObject:nextPageTapZone];            
+            [nextPageTapZone release];
+        }  
 		
 	}
 	
