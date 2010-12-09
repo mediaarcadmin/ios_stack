9e211075bad919b47878888e341477b27c38e930
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2010-12-09 21:48:04 +0000

    Two forgotten files...

---
+++ Sources/Classes/BlioDefaultViewController.h
@@ -0,0 +1,18 @@
+//
+//  BlioDefaultViewController.h
+//  BlioApp
+//
+//  Created by James Montgomerie on 09/12/2010.
+//  Copyright 2010 Things Made Out Of Other Things. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface BlioDefaultViewController : UIViewController {
+    BOOL viewOnScreen;
+}
+
+- (void)fadeOutDefaultImage;
+
+@end
