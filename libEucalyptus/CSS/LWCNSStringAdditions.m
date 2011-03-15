8f14c9a7dd6cd3d944e75ea5065a27676d926a0e
James Montgomerie <jamie@th.ingsmadeoutofotherthin.gs>
2011-03-15 16:26:29 +0000

    Replace embedded libEucalyptus with submodule (located at git@codebasehq.com:otherthings/libeucalyptus/libeucalyptus.git).

--- libEucalyptus/CSS/LWCNSStringAdditions.m
+++
@@ -1,9 +0,0 @@
-//
-//  LWCNSStringAdditions.m
-//  LibCSSTest
-//
-//  Created by James Montgomerie on 10/01/2010.
-//  Copyright 2010 Things Made Out Of Other Things. All rights reserved.
-//
-
-#import "LWCNSStringAdditions.h"
